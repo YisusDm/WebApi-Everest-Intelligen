8ca6e3e3865249a0131fb266a9e514189743dab3
Jesus Marquez <marquezortizjesusdavid@gmail.com>
2024-09-09 11:20:42 -0500

    inicio

---
+++ WebApi-Everest-Intelligent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApi_Everest_Intelligent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
